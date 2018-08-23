4c254f969fa26dd66efa8bb52efa18a99660e142
Wolle <26044260+schreibfaul1@users.noreply.github.com>
2018-08-23 08:24:08 +0200

    Add files via upload

--- IR/src/IR.cpp
+++ IR/src/IR.cpp
@@ -93,64 +93,71 @@ void IR::loop(){
 void IRAM_ATTR isr_IR()
 {
     extern IR ir;
-    int8_t ir_resp=(-1);
-    static uint32_t t0=0;                        // To get the interval
-    uint16_t ir_value = 0;                       // IR code
-    uint32_t t1, intval;                         // Current time and interval since last change
-    static uint32_t ir_locvalue=0;               // IR code
-    static int ir_loccount;                      // Length of code
-    uint32_t mask_in=2;                          // Mask input for conversion
-    uint16_t mask_out=1;                         // Mask output for conversion
-    t1=micros();                                 // Get current time
-    intval=t1 - t0;                              // Compute interval
-    t0=t1;                                       // Save for next compare
-    if((intval > 400) && (intval < 700)){        // Short pulse?
-        ir_locvalue=ir_locvalue << 1;            // Shift in a "zero" bit
-        ir_loccount++;                           // Count number of received bits
-    }
-    else if((intval > 1500) && (intval < 1700)){ // Long pulse?
 
-        ir_locvalue=(ir_locvalue << 1) + 1;      // Shift in a "one" bit
-        ir_loccount++;                           // Count number of received bits
+    int8_t          ir_resp=(-1);
+    uint16_t        address=0;                 // The first 4 bytes of IR code
+    uint16_t        command=0;                 // The last 4 bytes of IR code
+    uint32_t        t1, intval;                // Current time and interval since last change
+
+    static uint8_t  levelcounter=0;             // Counts the level changes
+    static uint8_t  pulsecounter=0;             // Counts the pulse
+    static uint32_t t0=0;                       // To get the interval
+    static uint32_t ir_value=0;                 // IR code
+    static boolean  ir_begin=false;             // set if HIGH/LOW change
+
+    t1=micros();                                // Get current time
+    intval=t1 - t0;                             // Compute interval
+    t0=t1;                                      // Save for next compare
+
+    if((intval>4000)&&(intval<5000)) {          // begin sequence of code?
+        pulsecounter=0;                         // Reset counter
+        ir_value=0;
+        levelcounter=0;
+        ir_begin=true;
+        return;
     }
-    else if(ir_loccount == 65){                  // Value is correct after 65 level changes
-        t0=0;
-        while(mask_in){                          // Convert 32 bits to 16 bits
-            if(ir_locvalue & mask_in) {          // Bit set in pattern?
-                ir_value|=mask_out;              // Set set bit in result
+
+    if(ir_begin==false) return;
+    levelcounter++;
+    if(levelcounter%2==1)return;                // only falling edge can pass
+
+    if(pulsecounter==32){
+        ir_begin=false;
+        ir_resp=(-1);
+        address=(ir_value&0xFFFF0000)>>16;
+        command=(ir_value&0x0000FFFF);
+        if(address==0x00FF){
+            switch(command){
+                case 19125: ir_resp=0;  break; //ZERO   0x4AB5
+                case 26775: ir_resp=1;  break; //ONE    0x6897
+                case 39015: ir_resp=2;  break; //TWO    0x9867
+                case 45135: ir_resp=3;  break; //THREE  0xB04F
+                case 12495: ir_resp=4;  break; //FOUR   0x30CF
+                case 6375 : ir_resp=5;  break; //FIVE   0x18E7
+                case 31365: ir_resp=6;  break; //SIX    0x7A85
+                case 4335 : ir_resp=7;  break; //SEVEN  0x10EF
+                case 14535: ir_resp=8;  break; //EIGHT  0x38C7
+                case 23205: ir_resp=9;  break; //NINE   0x5AA5
+                case 765:   ir_resp=10; break; //OK     0x02FD
+                case 25245: ir_resp=11; break; //UP     0x629D
+                case 43095: ir_resp=12; break; //DOWN   0xA857
+                case 49725: ir_resp=13; break; //RIGHT  0xC23D
+                case 8925:  ir_resp=14; break; //LEFT   0x22DD
+                case 21165: ir_resp=15; break; //HASH   0x52AD
+                case 17085: ir_resp=16; break; //STAR   0x42BD
+                default: ir_resp=(-1);
             }
-            mask_in<<=2;                         // Shift input mask 2 positions
-            mask_out<<=1;                        // Shift output mask 1 position
+            if(ir_resp>(-1))ir.setIRresult(ir_resp);
         }
-        ir_loccount=0;                           // Ready for next input
-        switch(ir_value){
-        case 19125: ir_resp=0;  break; //ZERO
-        case 26775: ir_resp=1;  break; //ONE
-        case 39015: ir_resp=2;  break; //TWO
-        case 45135: ir_resp=3;  break; //THREE
-        case 12495: ir_resp=4;  break; //FOUR
-        case 6375 : ir_resp=5;  break; //FIVE
-        case 31365: ir_resp=6;  break; //SIX
-        case 4335 : ir_resp=7;  break; //SEVEN
-        case 14535: ir_resp=8;  break; //EIGHT
-        case 23205: ir_resp=9;  break; //NINE
-        case 765:   ir_resp=10; break; //OK
-        case 25245: ir_resp=11; break; //UP
-        case 43095: ir_resp=12; break; //DOWN
-        case 49725: ir_resp=13; break; //RIGHT
-        case 8925:  ir_resp=14; break; //LEFT
-        case 21165: ir_resp=15; break; //HASH
-        case 17085: ir_resp=16; break; //STAR
-        default: ir_resp=(-1);
-        }
-        if(ir_resp>(-1))ir.setIRresult(ir_resp);
     }
-    else if((intval>4000)&&(intval<5000)) {
-        ir_loccount=0;          // Reset decoding
-        ir_locvalue=0;
+
+    if((intval > 500) && (intval < 700)){       // Short pulse?
+        ir_value=(ir_value << 1) + 0;           // Shift in a "zero" bit
+        pulsecounter++;                         // Count number of received bits
     }
-    else
-    {
-        // do nothing
+    if((intval > 1500) && (intval < 1700)){     // Long pulse?
+
+        ir_value=(ir_value << 1) + 1;           // Shift in a "one" bit
+        pulsecounter++;                         // Count number of received bits
     }
 }
